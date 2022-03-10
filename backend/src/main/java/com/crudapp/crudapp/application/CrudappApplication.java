19e7dfbdf6e76ad0295483d3b1d4e283e0f6bf66
kenfenheuer <timm.kenfenheuer@cbc.de>
2022-03-10 09:33:51 +0100

    init

--- backend/src/main/java/com/crudapp/crudapp/application/CrudappApplication.java
+++ backend/src/main/java/com/crudapp/crudapp/application/CrudappApplication.java
@@ -1,12 +1,16 @@
-package com.crudapp.crudapp;
+package com.crudapp.crudapp.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.crudapp.crudapp.model")
+@EnableJpaRepositories(basePackages = "com.crudapp.crudapp.repository")
 public class CrudappApplication {
 
     public static void main(String[] args) {
